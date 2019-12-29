13cd382d62d366e9a9ddbb999b62bbb74b3e9f2d
IamWahid <iam.wahidn@gmail.com>
2019-12-29 21:37:54 +0700

    detail page

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,6 +5,3 @@ final accent = const Color(0xFF2D9CDB);
 final cardcolor = const Color(0xfffcfbfc);
 final mygrey = const Color(0xffe5e5e8);
 final myshadow = const Color(0xFF3A5160);
-
-
-      
--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,22 +1,70 @@
+import 'package:final_project/colors.dart';
+import 'package:final_project/models/drink.dart';
 import 'package:flutter/material.dart';
 
-class DetailPage extends StatefulWidget {
-  @override
-  _DetailPageState createState() => _DetailPageState();
-}
+class DetailPage extends StatelessWidget {
+  final DrinkElement item;
+  DetailPage({Key key, this.item}) : super(key : key);
 
-class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
+    print(item.strDrinkThumb);
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
+        backgroundColor: primaryColor,
         leading: FlatButton(
-          child: Icon(Icons.arrow_back),
+          child: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: Navigator.of(context).pop,
         ),
       ),
-      body: Text('Halaman Detail'),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.network(item.strDrinkThumb),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(item.strDrink, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: accent
+                      ),
+                      child: Text(item.strCategory),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.bookmark),
+                    SizedBox(width: 5),
+                    Text(item.strTags != null ? item.strTags : 'No Tags'),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text("Instructions :", style: TextStyle(fontWeight: FontWeight.bold))
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(item.strInstructions)
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
