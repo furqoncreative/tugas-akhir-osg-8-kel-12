5554e5b40c266ff1329db078e613ac62d3162790
farhan0x1 <aangbahono@gmail.com>
2019-12-29 15:35:56 +0700

    tampilan home

--- lib/models/drink.dart
+++ lib/models/drink.dart
@@ -1,13 +1,229 @@
-class Drink {
-  final String name;
-  final String image;
-  final String category;
-  final String instruction;
+// To parse this JSON data, do
+//
+//     final drink = drinkFromJson(jsonString);
 
-  Drink({
-    this.name,
-    this.image,
-    this.category,
-    this.instruction,
-  });
+import 'dart:convert';
+
+Drink drinkFromJson(String str) => Drink.fromJson(json.decode(str));
+
+String drinkToJson(Drink data) => json.encode(data.toJson());
+
+class Drink {
+    List<DrinkElement> drinks;
+
+    Drink({
+        this.drinks,
+    });
+
+    factory Drink.fromJson(Map<String, dynamic> json) => Drink(
+        drinks: List<DrinkElement>.from(json["drinks"].map((x) => DrinkElement.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "drinks": List<dynamic>.from(drinks.map((x) => x.toJson())),
+    };
+}
+
+class DrinkElement {
+    String idDrink;
+    String strDrink;
+    dynamic strDrinkAlternate;
+    dynamic strDrinkEs;
+    dynamic strDrinkDe;
+    dynamic strDrinkFr;
+    dynamic strDrinkZhHans;
+    dynamic strDrinkZhHant;
+    dynamic strTags;
+    dynamic strVideo;
+    String strCategory;
+    dynamic strIba;
+    String strAlcoholic;
+    String strGlass;
+    String strInstructions;
+    String strDrinkThumb;
+    String strIngredient1;
+    String strIngredient2;
+    String strIngredient3;
+    String strIngredient4;
+    dynamic strIngredient5;
+    dynamic strIngredient6;
+    dynamic strIngredient7;
+    dynamic strIngredient8;
+    dynamic strIngredient9;
+    dynamic strIngredient10;
+    dynamic strIngredient11;
+    dynamic strIngredient12;
+    dynamic strIngredient13;
+    dynamic strIngredient14;
+    dynamic strIngredient15;
+    String strMeasure1;
+    String strMeasure2;
+    String strMeasure3;
+    String strMeasure4;
+    dynamic strMeasure5;
+    dynamic strMeasure6;
+    dynamic strMeasure7;
+    dynamic strMeasure8;
+    dynamic strMeasure9;
+    dynamic strMeasure10;
+    dynamic strMeasure11;
+    dynamic strMeasure12;
+    dynamic strMeasure13;
+    dynamic strMeasure14;
+    dynamic strMeasure15;
+    String strCreativeCommonsConfirmed;
+    DateTime dateModified;
+
+    DrinkElement({
+        this.idDrink,
+        this.strDrink,
+        this.strDrinkAlternate,
+        this.strDrinkEs,
+        this.strDrinkDe,
+        this.strDrinkFr,
+        this.strDrinkZhHans,
+        this.strDrinkZhHant,
+        this.strTags,
+        this.strVideo,
+        this.strCategory,
+        this.strIba,
+        this.strAlcoholic,
+        this.strGlass,
+        this.strInstructions,
+        this.strDrinkThumb,
+        this.strIngredient1,
+        this.strIngredient2,
+        this.strIngredient3,
+        this.strIngredient4,
+        this.strIngredient5,
+        this.strIngredient6,
+        this.strIngredient7,
+        this.strIngredient8,
+        this.strIngredient9,
+        this.strIngredient10,
+        this.strIngredient11,
+        this.strIngredient12,
+        this.strIngredient13,
+        this.strIngredient14,
+        this.strIngredient15,
+        this.strMeasure1,
+        this.strMeasure2,
+        this.strMeasure3,
+        this.strMeasure4,
+        this.strMeasure5,
+        this.strMeasure6,
+        this.strMeasure7,
+        this.strMeasure8,
+        this.strMeasure9,
+        this.strMeasure10,
+        this.strMeasure11,
+        this.strMeasure12,
+        this.strMeasure13,
+        this.strMeasure14,
+        this.strMeasure15,
+        this.strCreativeCommonsConfirmed,
+        this.dateModified,
+    });
+
+    factory DrinkElement.fromJson(Map<String, dynamic> json) => DrinkElement(
+        idDrink: json["idDrink"],
+        strDrink: json["strDrink"],
+        strDrinkAlternate: json["strDrinkAlternate"],
+        strDrinkEs: json["strDrinkES"],
+        strDrinkDe: json["strDrinkDE"],
+        strDrinkFr: json["strDrinkFR"],
+        strDrinkZhHans: json["strDrinkZH-HANS"],
+        strDrinkZhHant: json["strDrinkZH-HANT"],
+        strTags: json["strTags"],
+        strVideo: json["strVideo"],
+        strCategory: json["strCategory"],
+        strIba: json["strIBA"],
+        strAlcoholic: json["strAlcoholic"],
+        strGlass: json["strGlass"],
+        strInstructions: json["strInstructions"],
+        strDrinkThumb: json["strDrinkThumb"],
+        strIngredient1: json["strIngredient1"],
+        strIngredient2: json["strIngredient2"],
+        strIngredient3: json["strIngredient3"],
+        strIngredient4: json["strIngredient4"],
+        strIngredient5: json["strIngredient5"],
+        strIngredient6: json["strIngredient6"],
+        strIngredient7: json["strIngredient7"],
+        strIngredient8: json["strIngredient8"],
+        strIngredient9: json["strIngredient9"],
+        strIngredient10: json["strIngredient10"],
+        strIngredient11: json["strIngredient11"],
+        strIngredient12: json["strIngredient12"],
+        strIngredient13: json["strIngredient13"],
+        strIngredient14: json["strIngredient14"],
+        strIngredient15: json["strIngredient15"],
+        strMeasure1: json["strMeasure1"],
+        strMeasure2: json["strMeasure2"],
+        strMeasure3: json["strMeasure3"],
+        strMeasure4: json["strMeasure4"],
+        strMeasure5: json["strMeasure5"],
+        strMeasure6: json["strMeasure6"],
+        strMeasure7: json["strMeasure7"],
+        strMeasure8: json["strMeasure8"],
+        strMeasure9: json["strMeasure9"],
+        strMeasure10: json["strMeasure10"],
+        strMeasure11: json["strMeasure11"],
+        strMeasure12: json["strMeasure12"],
+        strMeasure13: json["strMeasure13"],
+        strMeasure14: json["strMeasure14"],
+        strMeasure15: json["strMeasure15"],
+        strCreativeCommonsConfirmed: json["strCreativeCommonsConfirmed"],
+        dateModified: DateTime.parse(json["dateModified"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "idDrink": idDrink,
+        "strDrink": strDrink,
+        "strDrinkAlternate": strDrinkAlternate,
+        "strDrinkES": strDrinkEs,
+        "strDrinkDE": strDrinkDe,
+        "strDrinkFR": strDrinkFr,
+        "strDrinkZH-HANS": strDrinkZhHans,
+        "strDrinkZH-HANT": strDrinkZhHant,
+        "strTags": strTags,
+        "strVideo": strVideo,
+        "strCategory": strCategory,
+        "strIBA": strIba,
+        "strAlcoholic": strAlcoholic,
+        "strGlass": strGlass,
+        "strInstructions": strInstructions,
+        "strDrinkThumb": strDrinkThumb,
+        "strIngredient1": strIngredient1,
+        "strIngredient2": strIngredient2,
+        "strIngredient3": strIngredient3,
+        "strIngredient4": strIngredient4,
+        "strIngredient5": strIngredient5,
+        "strIngredient6": strIngredient6,
+        "strIngredient7": strIngredient7,
+        "strIngredient8": strIngredient8,
+        "strIngredient9": strIngredient9,
+        "strIngredient10": strIngredient10,
+        "strIngredient11": strIngredient11,
+        "strIngredient12": strIngredient12,
+        "strIngredient13": strIngredient13,
+        "strIngredient14": strIngredient14,
+        "strIngredient15": strIngredient15,
+        "strMeasure1": strMeasure1,
+        "strMeasure2": strMeasure2,
+        "strMeasure3": strMeasure3,
+        "strMeasure4": strMeasure4,
+        "strMeasure5": strMeasure5,
+        "strMeasure6": strMeasure6,
+        "strMeasure7": strMeasure7,
+        "strMeasure8": strMeasure8,
+        "strMeasure9": strMeasure9,
+        "strMeasure10": strMeasure10,
+        "strMeasure11": strMeasure11,
+        "strMeasure12": strMeasure12,
+        "strMeasure13": strMeasure13,
+        "strMeasure14": strMeasure14,
+        "strMeasure15": strMeasure15,
+        "strCreativeCommonsConfirmed": strCreativeCommonsConfirmed,
+        "dateModified": dateModified.toIso8601String(),
+    };
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:final_project/colors.dart';
+import 'package:final_project/models/drink.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,21 +12,118 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<Drink> getData() async {
+    final response = await http.get(
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
+        headers: {"Accept": "application/json"});
+
+    if (response.statusCode == 200) {
+      return drinkFromJson(response.body);
+    } else {
+      throw Exception("Data Failed To Load");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    double defaultScreenWidth = 400.0;
+    double defaultScreenHeight = 810.0;
+    ScreenUtil.instance = ScreenUtil(
+      width: defaultScreenWidth,
+      height: defaultScreenHeight,
+      allowFontScaling: true,
+    )..init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Drinks'),
+        backgroundColor: primaryColor,
         actions: <Widget>[
           FlatButton(
             child: Icon(Icons.info, color: Colors.white),
             onPressed: () {
               Navigator.of(context).pushNamed('/about');
-            }, 
+            },
           )
         ],
       ),
-      body: Text('Hello'),
+      body: FutureBuilder<Drink>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          Drink drink = snapshot.data;
+          if (snapshot.hasData) {
+            return GridView.builder(
+              padding: EdgeInsets.all(ScreenUtil.instance.setWidth(15.0)),
+              itemCount: drink.drinks.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                childAspectRatio: 0.8,
+              ),
+              scrollDirection: Axis.vertical,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return Align(
+                  alignment: Alignment.center,
+                  child: InkWell(
+                    onTap: () {},
+                    child: SizedBox(
+                        height: ScreenUtil.instance.setHeight(280.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: primaryColor),
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                drink.drinks[index].strDrink,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: primaryColor),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      top: ScreenUtil.instance.setHeight(24),
+                                      right: ScreenUtil.instance.setHeight(16),
+                                      left: ScreenUtil.instance.setHeight(16)),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(16.0)),
+                                      boxShadow: <BoxShadow>[
+                                        BoxShadow(
+                                            color: myshadow.withOpacity(0.2),
+                                            offset: const Offset(0.0, 0.0),
+                                            blurRadius: 6.0),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(20.0)),
+                                        child: AspectRatio(
+                                          aspectRatio: 1.20,
+                                          child: Image.network(drink
+                                              .drinks[index].strDrinkThumb),
+                                        )),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                );
+              },
+            );
+          }
+          return Container(child: Center(child: CircularProgressIndicator()));
+        },
+      ),
     );
   }
 }
